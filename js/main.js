acb5882939b58916fc2fdaefd0b807b6dc54a7a4
Bhakti Al Akbar <balapa95@gmail.com>
2019-05-28 16:43:56 +0700

    add more animation on homepage and currently working on popup

--- js/main.js
+++ js/main.js
@@ -1,18 +1,38 @@
+// utils
+function toArray(nodelist) {
+	return [].slice.call(nodelist);
+}
+
+///////////////////////
+
 var header = document.querySelector('header');
+var burgerMenu = document.querySelector('.burger-menu');
 var body = document.body;
 
-window.addEventListener('scroll', function() {
+// header
+function headerInteraction() {
 	var scrollTop = window.pageYOffset || document.documentElement.scrollTop;
 	if( scrollTop > 0 ) {
 		header.classList.add('show-bg');
 	} else {
 		header.classList.remove('show-bg');
 	}
+}
+
+headerInteraction();
+
+window.addEventListener('scroll', function() {
+	headerInteraction();
+});
+
+// burger menu
+burgerMenu.addEventListener('click', function() {
+	header.classList.toggle('show-mobile-menu');
 });
 
 body.addEventListener('keyup', function(e) {
 	if(e.keyCode == 27) {
-		closePreview();
+
 	}
 })
 
@@ -67,3 +87,76 @@ heroTL
 		delay: .5,
 		x: '0%'
 	})
+
+// scroll animations
+function animFly(items, options) {
+	if(options.state == 'forward') {
+
+		TweenMax.staggerFrom(items, options.duration, {
+			delay: options.delay,
+			y: options.y,
+			ease: Power4.easeOut
+		}, options.repeatDelay)
+		TweenMax.staggerTo(items, options.duration, {
+			delay: options.delay,
+			opacity: 1
+		}, options.repeatDelay)
+
+	} else {
+
+		TweenMax.staggerTo(items, options.duration, {
+			opacity: 0,
+			y: options.y * -1
+		}, options.repeatDelay)
+
+	}
+}
+
+// stagger animation
+var staggerAnims = document.querySelectorAll('.stagger-animation');
+var yAnim = 15;
+if(staggerAnims) {
+
+	staggerAnims = toArray(staggerAnims);
+	staggerAnims.forEach(function(anim) {
+		var animItems = anim.querySelectorAll('.anim-item');
+		animItems = toArray(animItems);
+
+		var animWatcher = scrollMonitor.create(anim, -250);
+		var animScroll = anim.getAttribute('data-scroll');
+		var animState = false;
+
+		if(animScroll == 'false') {
+
+			animFly(animItems, {
+				state: 'forward',
+				duration: .85,
+				delay: .3,
+				y: yAnim,
+				repeatDelay: .2
+			})
+			animState = true;
+
+		} else {
+
+			animWatcher.enterViewport(function() {
+				if(!animState) {
+					animFly(animItems, {
+						state: 'forward',
+						duration: .85,
+						delay: 0,
+						y: yAnim,
+						repeatDelay: .2
+					})
+					animState = true;
+				}
+			})
+
+		}
+
+		animWatcher.exitViewport(function() {
+			console.log('exit');
+		})
+	});
+
+}
